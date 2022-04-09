273be167517691ad245dd7db53dd7b61e7f3aab7
jojida <46383309+jojida@users.noreply.github.com>
2022-04-09 11:50:23 +0300

    Animation

--- script.js
+++ script.js
@@ -38,13 +38,18 @@ window.onload = () =>{
 
 }
 
-document.querySelector('home').onmousemove = (e) =>{
-    document.querySelectorAll('.home-parallax').forEach(elm =>{
-        let speed = elm.getAttribute('data-speed');
-        let x = (window.innerWidth - e.pageX * speed) / 90;
-        let y = (window.innerHeight - e.pageY * speed) / 90;
+document.querySelector('.home').onmousemove = (e) =>{
 
-        elm.style.transform = `translateX(${y}px) translateY(${x}px)`;
-        
+    document.querySelectorAll('.home-parallax').forEach(elm =>{
+  
+      let speed = elm.getAttribute('data-speed');
+  
+      let x = (window.innerWidth - e.pageX * speed) / 90;
+      let y = (window.innerHeight - e.pageY * speed) / 90;
+  
+      elm.style.transform = `translateX(${y}px) translateY(${x}px)`;
+  
     });
-}
+  
+  };
+
